7a66f0547be48f062e764ba05c69affd275ee4b1
ZeroxDev <zeroxxdev@gmail.com>
2025-01-20 21:39:31 +0300

    Add files via upload

---
+++ TB.py
@@ -0,0 +1,9 @@
+import os
+
+os.chdir("/Users/zeroxdev/Downloads/turbo-boost-disable-main")
+
+os.system("clear")
+os.system("chmod +x load.sh")
+os.system("./load.sh")
+
+done = input("Операция прошла успешно! Нажмите Enter для закрытия окна ")
